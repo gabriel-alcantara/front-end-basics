102020a982fa4ebe6a83be75ff471bacddbf768e
gabrielalcantara <gabrielalcantara@wizsolucoes.com.br>
2019-12-11 03:37:50 -0300

    listing all modes correctly

--- jsfile.js
+++ jsfile.js
@@ -1,21 +1,62 @@
-// let i = 0;
-// while (i < 3) { // shows 0, then 1, then 2
-//     alert(i);
-//     i++;
+// function validateInput(valueInput) {
+//     var reg = new RegExp("^([1-8][0-9])|(9[0-9])|(100)$");
+    
+//     console.log('#', valueInput)
+//     if (reg.test(valueInput)) {
+//         console.log("Valid");
+//         getRandomNumbers(validateInput);
+//     } else {
+//         console.log("invalid value");
+//         document.getElementById("inputNumber").focus();
+//     }
+
 // }
 
-document.body.style.background = 'red';
 
-setTimeout(() => document.body.style.background = '', 1000);
+var randomNumbers = []; 
 
-console.log(document.getElementById("inputNumber").value);
+function getRandomNumbers(qtd) {
+    console.log('value inserted: ', qtd);
 
-var randomNumbers = [];
+    randomNumbers = []; // zerando array
+    // var ul = document.createElement("UL");
 
-function myFunc(valueInserted) {
-    console.log('value inserted: ', valueInserted);
-    for (let index = 0; index < valueInserted; index++) {
-        // alert(Math.round(Math.random()*100));
-        document.write(Math.round(Math.random() * 100) + " ");
+    for (let index = 0; index < qtd; index++) {
+        
+        // var li = document.createElement('LI');
+        randomNumbers[index] = Math.round(Math.random() * 100);
+        // li.appendChild(document.createTextNode(randomNumbers[index]));
+        // ul.appendChild(li);
+        // document.body.appendChild(ul);
+
     }
+
+    var para = document.createElement("P");
+
+    para.innerHTML = randomNumbers;                 
+    document.body.appendChild(para);
+    document.getElementById("random").appendChild(para);
+
+    var para = document.createElement("P");   
+    para.innerHTML = randomNumbers.sort();
+    document.body.appendChild(para);
+    document.getElementById("sorted").appendChild(para);
+
+    var para = document.createElement("P");   
+    para.innerHTML = randomNumbers.sort((a, b) => b-a );
+    document.body.appendChild(para);
+    document.getElementById("reverseSorted").appendChild(para);
+
+    var para = document.createElement("P");   
+    para.innerHTML = randomNumbers.filter(num => num % 2 == 0 && num !== 0).sort((a, b) => b-a );
+    document.body.appendChild(para);
+    document.getElementById("even").appendChild(para);
+
+    var para = document.createElement("P");   
+    para.innerHTML = randomNumbers.filter(num => num % 2 !== 0 && num !== 0).sort();
+    document.body.appendChild(para);
+    document.getElementById("odd").appendChild(para);
+
+
+    console.log("qtd", randomNumbers);
 }
