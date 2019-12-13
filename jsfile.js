1239798b8c854639dece4bb646629b59df28800a
gabrielalcantara <gabrielalcantara@wizsolucoes.com.br>
2019-12-13 01:19:58 -0300

    cleaning js file and fixing validate input

--- jsfile.js
+++ jsfile.js
@@ -1,63 +1,43 @@
+// validate range in input and type 
 function validateInput(valueInput) {
-    var reg = new RegExp("[1-9][0-9]");
-    console.log(document.readyState);
-    
-    if (reg.test(valueInput)) {
-        console.log("Valid");
+
+    if (!isNaN(valueInput) && valueInput >= 10 && valueInput <= 100) {
         getRandomNumbers(valueInput);
     } else {
-        console.log("invalid value");
         document.getElementById("inputNumber").focus();
+        alert("Entrada inválida, digite um número entre 10 e 100.");
     }
 
 }
 
-
-var randomNumbers = []; 
-
-var cont = 0;
-
+// generate random number and add result to div
 function getRandomNumbers(qtd) {
-    console.log('value inserted: ', qtd);
-    
-    randomNumbers = []; // zerando array
-    // var ul = document.createElement("UL");
+
+    var randomNumbers = []; // initializing array
 
     for (let index = 0; index < qtd; index++) {
-        
-        // var li = document.createElement('LI');
-        randomNumbers[index] = Math.round(Math.random() * 100);
-        // li.appendChild(document.createTextNode(randomNumbers[index]));
-        // ul.appendChild(li);
-        // document.body.appendChild(ul);
-
+        randomNumbers[index] = Math.round(Math.random() * 100); // adding random number generated to array
     }
 
+    addContent(randomNumbers, "random");
+
+    var sortedNumbers = randomNumbers.sort((a, b) => a-b);
+    addContent(sortedNumbers, "sorted");
+
+    var reverseSortedNumbers = randomNumbers.sort((a, b) => b-a);
+    addContent(reverseSortedNumbers, "reverseSorted");
+
+    var evenNumbers = randomNumbers.filter(num => num % 2 == 0 && num !== 0).sort((a, b) => b-a );
+    addContent(evenNumbers, "even");
+
+    var oddNumbers = randomNumbers.filter(num => num % 2 !== 0 && num !== 0).sort((a, b) => a-b);
+    addContent(oddNumbers, "odd");
+
+}
+
+// add at result div, for each requisite: random, sorted, inverse sorted, odd and even
+function addContent(numbers, nameDiv){
     var para = document.createElement("P");
-    para.innerHTML = randomNumbers;                 
-    // document.body.appendChild(para);
-    document.getElementById("random").appendChild(para);
-
-    var para = document.createElement("P");   
-    para.innerHTML = randomNumbers.sort();
-    // document.body.appendChild(para);
-    document.getElementById("sorted").appendChild(para);
-
-    var para = document.createElement("P");   
-    para.innerHTML = randomNumbers.sort((a, b) => b-a );
-    // document.body.appendChild(para);
-    document.getElementById("reverseSorted").appendChild(para);
-
-    var para = document.createElement("P");   
-    para.innerHTML = randomNumbers.filter(num => num % 2 == 0 && num !== 0).sort((a, b) => b-a );
-    // document.body.appendChild(para);
-    document.getElementById("even").appendChild(para);
-
-    var para = document.createElement("P");   
-    para.innerHTML = randomNumbers.filter(num => num % 2 !== 0 && num !== 0).sort();
-    // document.body.appendChild(para);
-    document.getElementById("odd").appendChild(para);
-
-
-    console.log("qtd", randomNumbers);
+    para.innerHTML = numbers;
+    document.getElementById(nameDiv).appendChild(para);
 }
